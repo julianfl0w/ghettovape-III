b594a08e205cc6d79dd97c6f1ddfef2049769c7a
jcloiacon <julian3@umbc.edu>
2015-06-22 20:57:09 -0400

    some corrections to comments

--- vape.ino
+++ vape.ino
@@ -5,40 +5,22 @@
 
 
 /*
-  LiquidCrystalFast Library - Cursor
- 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystalFast
- library works with all LCD displays that are compatible with the 
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
- 
- This sketch prints "Hello World!" to the LCD and
- uses the cursor()  and noCursor() methods to turn
- on and off the cursor.
  
  The circuit:
- * LCD RS pin to digital pin 12
- * LCD WR pin to digital pin 10
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * 10K resistor:
+ * Switch between +3.7v and 5V boost converter
+ * Switch between +5v and pin 10
+ 
+ * LCD RS pin to digital pin 9
+ * LCD WR pin to digital pin 8
+ * LCD Enable pin to digital pin 7
+ * LCD D4 pin to digital pin 6
+ * LCD D5 pin to digital pin 5
+ * LCD D6 pin to digital pin 4
+ * LCD D7 pin to digital pin 3
+ * 10K potentiometer:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
  
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe 
- modified 25 July 2009
- by David A. Mellis
- 
- http://www.pjrc.com/teensy/td_libs_LiquidCrystal.html
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
  */
 
 // include the library code:
