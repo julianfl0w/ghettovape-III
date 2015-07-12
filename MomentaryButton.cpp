87bb4c39e17a902ea69772e763903956ca857dce
jcloiacon <julian3@umbc.edu>
2015-07-12 10:04:37 -0400

    Create MomentaryButton.cpp

---
+++ MomentaryButton.cpp
@@ -0,0 +1,63 @@
+/*
+
+Put this file in your libraries folder along with MomentaryButton.h
+
+Written by Mark Fickett
+Modified by Julian Loiacono December '14
+*/
+
+#include "MomentaryButton.h"
+
+
+MomentaryButton::MomentaryButton(int inputPin)
+	: wasClosed(false), pin(inputPin)
+{}
+
+void MomentaryButton::setThreshold(unsigned long newThreshold)
+{
+	holdThreshold = newThreshold;
+}
+
+void MomentaryButton::setup()
+{
+	pinMode(pin, INPUT);
+	digitalWrite(pin, HIGH);
+}
+
+void MomentaryButton::check()
+{
+	unsigned long currentTimeMillis = millis();
+	boolean isClosed = (digitalRead(pin) == LOW);
+
+	if (isClosed & !wasClosed)
+	{
+		closeTimeMillis = currentTimeMillis;
+	}
+	else if (!isClosed & wasClosed)
+	{
+		held = (currentTimeMillis - closeTimeMillis) >= holdThreshold;
+		clicked = (!held && (currentTimeMillis - closeTimeMillis) >= bounceThreshold);
+	}
+
+	wasClosed = isClosed;
+}
+
+boolean MomentaryButton::wasClicked() {
+	if (clicked){
+		clicked = false;
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+boolean MomentaryButton::wasHeld() {
+	if (held){
+		held = false;
+		return true;
+	}
+	else{
+		return false;
+	}
+}
